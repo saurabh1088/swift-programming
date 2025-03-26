1e243a0ac7628a552f312c58b3e2a51b1e92d1ba
Saurabh Verma <saurabh1088@gmail.com>
2025-03-26 23:04:01 +0530

    Added documenataion to method and updated method name.

--- Sources/swift-programming/Anagram.swift
+++ Sources/swift-programming/Anagram.swift
@@ -9,7 +9,16 @@ import Foundation
 
 struct Anagram {
     
-    static func areAnaagrams(_ string1: String, _ string2: String) -> Bool {
+    /// Checks if two given strings are anagrams of each other.
+    ///
+    /// An anagram is a word or phrase formed by rearranging the letters of another.
+    /// This method sorts both input strings and compares them to determine if they contain the same characters in the same frequency.
+    ///
+    /// - Parameters:
+    ///   - string1: The first string to compare.
+    ///   - string2: The second string to compare.
+    /// - Returns: A Boolean value indicating whether the two strings are anagrams.
+    static func areAnaagramsUsingSimpleSortCompare(_ string1: String, _ string2: String) -> Bool {
         let sortedString1 = string1.sorted().reduce("") { partialResult, character in
             partialResult + String(character)
         }
