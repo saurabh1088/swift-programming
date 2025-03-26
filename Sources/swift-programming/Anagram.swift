b8d084ef23a99a263e4d7d8c32fac1fd413f577e
Saurabh Verma <saurabh1088@gmail.com>
2025-03-26 22:55:16 +0530

    Added first example for swift programming.

---
+++ Sources/swift-programming/Anagram.swift
@@ -0,0 +1,21 @@
+//
+//  Anagram.swift
+//  swift-programming
+//
+//  Created by Saurabh Verma on 26/03/25.
+//
+
+import Foundation
+
+struct Anagram {
+    
+    static func areAnaagrams(_ string1: String, _ string2: String) -> Bool {
+        let sortedString1 = string1.sorted().reduce("") { partialResult, character in
+            partialResult + String(character)
+        }
+        let sortedString2 = string2.sorted().reduce("") { partialResult, character in
+            partialResult + String(character)
+        }
+        return sortedString1 == sortedString2
+    }
+}
