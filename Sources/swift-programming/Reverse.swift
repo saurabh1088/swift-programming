ef89ac8d4f3897f9980e19da4c5aa2cd57015f70
Saurabh Verma <saurabh1088@gmail.com>
2025-04-06 19:41:26 +0530

    Added file to add functions for reversing a string.

---
+++ Sources/swift-programming/Reverse.swift
@@ -0,0 +1,15 @@
+//
+//  Reverse.swift
+//  swift-programming
+//
+//  Created by Saurabh Verma on 06/04/25.
+//
+
+import Foundation
+
+struct Reverse {
+    
+    static func reverseString(_ input: String) -> String {
+        return String(input.reversed())
+    }
+}
