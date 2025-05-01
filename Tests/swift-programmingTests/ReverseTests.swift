aae98685a7c9ee323a10d0e4ea4ae5e08223780f
Saurabh Verma <saurabh1088@gmail.com>
2025-05-01 08:41:05 +0530

    Added unit tests for method reverseString.

--- Tests/swift-programmingTests/ReverseTests.swift
+++ Tests/swift-programmingTests/ReverseTests.swift
@@ -1,8 +1,23 @@
 //
-//  File.swift
+//  ReverseTests.swift
 //  swift-programming
 //
 //  Created by Saurabh Verma on 01/05/25.
 //
 
-import Foundation
+import XCTest
+@testable import swift_programming
+
+final class ReverseTests: XCTestCase {
+    
+    func test_reverseString_withEmptyString() {
+        let inputString = ""
+        XCTAssertEqual(Reverse.reverseString(inputString), inputString)
+    }
+    
+    func test_reverseString() {
+        let inputString = "Hello World"
+        let reversedString = "dlroW olleH"
+        XCTAssertEqual(Reverse.reverseString(inputString), reversedString)
+    }
+}
