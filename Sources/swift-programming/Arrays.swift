0e214a22bfca2fa42d7069aef52a593eca8f1137
Saurabh Verma <saurabh1088@gmail.com>
2025-07-19 20:29:33 +0530

    Fixed issue with empty lists.

--- Sources/swift-programming/Arrays.swift
+++ Sources/swift-programming/Arrays.swift
@@ -9,7 +9,8 @@ import Foundation
 
 struct Arrays {
     
-    static func largestElement(in array: [Int]) -> Int {
+    static func largestElement(in array: [Int]) -> Int? {
+        guard array.isEmpty == false else { return nil }
         var largestElement = array[0]
         for item in array {
             if item > largestElement {
