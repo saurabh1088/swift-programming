40f25863289d07c428be82bd0f75026562b04459
Saurabh Verma <saurabh1088@gmail.com>
2025-12-17 19:24:44 +0530

    Added SlidingWindow.swift to try sliding window examples and concepts.

---
+++ Sources/swift-programming/SlidingWindow.swift
@@ -0,0 +1,34 @@
+//
+//  SlidingWindow.swift
+//  swift-programming
+//
+//  Created by Saurabh Verma on 17/12/25.
+//
+
+import Foundation
+
+struct SlidingWindow {
+    
+    static func maximumSubArraySum() {
+        let array = [2, 3, 1, 2, 4, 3]
+        var currentMax = 0
+        var max = 0
+        var window = 3
+        
+        for index in 0..<3 {
+            currentMax = currentMax + array[index]
+        }
+        
+        for (index, num) in array.enumerated() {
+            if index + window < array.count {
+                currentMax = currentMax - array[index]
+                currentMax = currentMax + array[index + window]
+                if currentMax > max {
+                    max = currentMax
+                }
+            }
+        }
+        
+        print("Maximum sum : \(max)")
+    }
+}
