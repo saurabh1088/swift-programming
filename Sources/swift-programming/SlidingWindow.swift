2bfa7317cca1a63687639023a81301e6c9fb46c3
Saurabh Verma <saurabh1088@gmail.com>
2025-12-18 19:34:47 +0530

    Updated to resolve warnings.

--- Sources/swift-programming/SlidingWindow.swift
+++ Sources/swift-programming/SlidingWindow.swift
@@ -13,13 +13,13 @@ struct SlidingWindow {
         let array = [2, 3, 1, 2, 4, 3]
         var currentMax = 0
         var max = 0
-        var window = 3
+        let window = 3
         
         for index in 0..<3 {
             currentMax = currentMax + array[index]
         }
         
-        for (index, num) in array.enumerated() {
+        for (index, _) in array.enumerated() {
             if index + window < array.count {
                 currentMax = currentMax - array[index]
                 currentMax = currentMax + array[index + window]
