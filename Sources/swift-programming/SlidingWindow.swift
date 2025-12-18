3f5bd58b42bc8a438859dedbf25061bc89257031
Saurabh Verma <saurabh1088@gmail.com>
2025-12-18 20:22:32 +0530

    Minor refactoring.

--- Sources/swift-programming/SlidingWindow.swift
+++ Sources/swift-programming/SlidingWindow.swift
@@ -10,25 +10,28 @@ import Foundation
 struct SlidingWindow {
     
     static func maximumSubArraySum() {
-        let array = [2, 3, 1, 2, 4, 3]
+        let array = [1, 2, 3, 4, 9, 6, 7, 8, 3, 4, 7, 2, 1, 5, 8, 9, 4, 5, 6, 7]
         var currentMax = 0
-        var max = 0
-        let window = 3
+        var finalMax = 0
+        let windowSize = 3
+        let startingIndex = windowSize - 1
+        let lastIndex = array.count - 1
         
-        for index in 0..<3 {
+        for index in 0..<windowSize {
             currentMax = currentMax + array[index]
         }
         
-        for (index, _) in array.enumerated() {
-            if index + window < array.count {
-                currentMax = currentMax - array[index]
-                currentMax = currentMax + array[index + window]
-                if currentMax > max {
-                    max = currentMax
-                }
+        finalMax = currentMax
+        
+        for index in startingIndex..<lastIndex {
+            print("Iteration :: \(index) | Current max :: \(currentMax) | Final max :: \(finalMax)")
+            currentMax = currentMax - array[index - windowSize + 1]
+            currentMax = currentMax + array[index + 1]
+            if currentMax > finalMax {
+                finalMax = currentMax
             }
         }
         
-        print("Maximum sum : \(max)")
+        print("Maximum sum : \(finalMax)")
     }
 }
