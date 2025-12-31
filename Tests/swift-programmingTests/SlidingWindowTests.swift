b7e994a8f2020a6a91365b3514d86d5acc1f46ff
Saurabh Verma <saurabh1088@gmail.com>
2025-12-31 19:45:42 +0530

    Added tests for SlidingWindow maximumSubArraySum.

---
+++ Tests/swift-programmingTests/SlidingWindowTests.swift
@@ -0,0 +1,21 @@
+//
+//  SlidingWindowTests.swift
+//  swift-programming
+//
+//  Created by Saurabh Verma on 31/12/25.
+//
+
+import XCTest
+@testable import swift_programming
+
+final class SlidingWindowTests: XCTestCase {
+    
+    func test_maximumSubArraySum_returnsCorrectSum() {
+        let input: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 4, 2, 5, 7, 23, 6, 7, 9, 5, 6, 7, 1]
+        let expected: Int = 36
+        
+        let result = SlidingWindow.maximumSubArraySum(for: input, of: 3)
+        
+        XCTAssertEqual(result, expected)
+    }
+}
